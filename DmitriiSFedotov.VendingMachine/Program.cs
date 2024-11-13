4f160d589450eb98e21aace71eb9cb54fd35da41
dmitriisfedotov <dmitriisfedotov@icloud.com>
2024-11-13 23:31:39 +0300

    Refactoring

--- DmitriiSFedotov.VendingMachine/Program.cs
+++ DmitriiSFedotov.VendingMachine/Program.cs
@@ -1,107 +1,95 @@
-﻿namespace DmitriiSFedotov.VendingMachine;
-
-internal class Program
+﻿while (true)
 {
-    static void Main()
+    string[] productNames = { "Добрый Кола", "Вода Черноголовка", "Сок Добрый", "Чипсы Русская Картошка", "Печенье Овсяное" };
+
+    decimal[] productPrices = { 100, 80, 120, 100, 60 };
+
+    for (int i = 0; i < productNames.Length; i++)
     {
-        while (true)
+        Console.WriteLine($"{i + 1}. {productNames[i]} - {productPrices[i]}");
+    }
+
+    int selectedIndex = ReadFromConsole("\nВыберите товар: ") - 1;
+
+    if (selectedIndex < 0 || selectedIndex > productNames.Length - 1)
+    {
+        Console.WriteLine("Некорректный выбор! Попробуйте снова!");
+        continue;
+    }
+
+    Console.WriteLine($"\nВы выбрали {productNames[selectedIndex]} \nЦена: {productPrices[selectedIndex]} ");
+
+    List<decimal> allowedBanknotes = new List<decimal>() { 5, 10, 50, 100, 500 };
+
+    decimal currentSum = 0;
+
+    while (true)
+    {
+
+        Console.WriteLine("\nАвтомат принимает купюры номиналом: ");
+
+        for (int i = 0; i < allowedBanknotes.Count; i++)
         {
-            Console.Clear();
-
-            string[] productNames = { "Добрый Кола", "Вода Черноголовка", "Сок Добрый", "Чипсы Русская Картошка", "Печенье Овсяное" };
-
-            decimal[] productPrices = { 100, 80, 120, 100, 60 };
-
-            for (int i = 0; i < productNames.Length; i++)
-            {
-                Console.WriteLine($"{i + 1}. {productNames[i]} - {productPrices[i]}");
-            }
-
-            int selectedIndex = ReadFromConsole("\nВыберите товар: ") - 1;
-
-            if (selectedIndex < 0 || selectedIndex > productNames.Length - 1)
-            {
-                Console.WriteLine("Некорректный выбор! Попробуйте снова!");
-                continue;
-            }
-
-            Console.WriteLine($"\nВы выбрали {productNames[selectedIndex]} \nЦена: {productPrices[selectedIndex]} ");
-
-            List<decimal> allowedBanknotes = new List<decimal>() { 5, 10, 50, 100, 500 };
-
-            decimal currentSum = 0;
-
-            while (true)
-            {
-
-                Console.WriteLine("\nАвтомат принимает купюры номиналом: ");
-
-                for (int i = 0; i < allowedBanknotes.Count; i++)
-                {
-                    string m = i == allowedBanknotes.Count - 1
-                        ? allowedBanknotes[i].ToString()
-                        : $"{allowedBanknotes[i]}, ";
-                    Console.Write(m);
-                }
-
-                decimal money = ReadFromConsole("\nВнесите купюру: ");
-
-                if (money == 0)
-                {
-                    currentSum = 0;
-                    break;
-                }
-
-                if (!allowedBanknotes.Contains(money))   // метод Contains проверяет содержит ли коллекция элемент
-                {
-                    Console.WriteLine("\nКупюра не распознана! Внесите другую!");
-                    continue;
-                }
-                else
-                {
-                    currentSum = currentSum + money;
-                    Console.WriteLine($"\nТекущая сумма: {currentSum}");
-                }
-
-                if (currentSum >= productPrices[selectedIndex])
-                {
-                    Console.WriteLine($"\nЗаберите {productNames[selectedIndex]}");
-
-                    decimal change = currentSum - productPrices[selectedIndex];
-
-                    if (change > 0)
-                    {
-                        Console.WriteLine($"Возьмите сдачу: {change}");
-                    }
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Внесите еще!");
-                }           
-            }     
-            Console.ReadLine();
+            string m = i == allowedBanknotes.Count - 1
+                ? allowedBanknotes[i].ToString()
+                : $"{allowedBanknotes[i]}, ";
+            Console.Write(m);
         }
 
-        return;
+        decimal money = ReadFromConsole("\nВнесите купюру: ");
 
-        static int ReadFromConsole(string message)
+        if (money == 0)
         {
-            bool parsed;
-            int elementsCount;
+            currentSum = 0;
+            break;
+        }
 
-            Console.Write(message);
+        if (!allowedBanknotes.Contains(money))   // метод Contains проверяет содержит ли коллекция элемент
+        {
+            Console.WriteLine("\nКупюра не распознана! Внесите другую!");
+            continue;
+        }
+        else
+        {
+            currentSum = currentSum + money;
+            Console.WriteLine($"\nТекущая сумма: {currentSum}");
+        }
 
-            do
+        if (currentSum >= productPrices[selectedIndex])
+        {
+            Console.WriteLine($"\nЗаберите {productNames[selectedIndex]}");
+
+            decimal change = currentSum - productPrices[selectedIndex];
+
+            if (change > 0)
             {
-                string input = Console.ReadLine();
-                parsed = int.TryParse(input, out elementsCount);
-                if (!parsed)
-                {
-                    Console.WriteLine("Некорректный выбор! Попробуйте снова!");
-                }
-            } while (!parsed);
-            return elementsCount;
+                Console.WriteLine($"Возьмите сдачу: {change}");
+            }
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Внесите еще!");
         }
     }
+    Console.ReadLine();
+}
+
+static int ReadFromConsole(string message)
+{
+    bool parsed;
+    int elementsCount;
+
+    Console.Write(message);
+
+    do
+    {
+        string? input = Console.ReadLine();
+        parsed = int.TryParse(input, out elementsCount);
+        if (!parsed)
+        {
+            Console.WriteLine("Некорректный выбор! Попробуйте снова!");
+        }
+    } while (!parsed);
+    return elementsCount;
 }
